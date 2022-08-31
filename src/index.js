9838772b249c860bbf6cfaf6db630f7275c00769
Kyle Chinick <kylechinick@icloud.com>
2022-08-31 16:09:22 -0700

    Add support to display character stats to user
    Co-authored-by: alex-mcknight11 <alex-mcknight11@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ const storeState = initialValue => {
   };
 };
 
-const stateControl = storeState();
-const stateControl2 = storeState();
-const stateControl3 = storeState();
-
 const changeState = prop => {
   return value => {
     return state => ({
@@ -30,37 +26,22 @@ const changeState = prop => {
 const poorlyCastMageLight = changeState('hp')(-5);
 const poorlyCastFireball = changeState('hp')(-10);
 const healingSpell = changeState('hp')(5);
-// TO SOLVE: add points to 'hp' AND remove points from 'magic'
-// const healingSpell = changeState('hp')(5)('magic')(-5);
+// TO SOLVE: add points to 'hp' AND remove points from 'magic' at the same time --> ? > const healingSpell = changeState('hp')(5)('magic')(-5);
 
 // UI
 
 $(document).ready(function () {
-  let wizards = [];
-  $('#player-choice-form').submit(function (event) {
-    event.preventDefault();
-    let playerChoice = $('input[type=radio][name=survey]:checked').val();
-    console.log(playerChoice);
-    $('#fighter-choice').text(playerChoice);
-
-    const playerGenerator = () => {
-      const wizard1 = storeState(wizard);
-      wizards.push(wizard1);
-      console.log(wizard1());
-      $('#player-stats-hp').text(wizard1().hp);
-      $('#player-stats-magic').text(wizard1().magic);
-      $('#player-stats-water').text(wizard1().water);
-      $('#player-stats-hp').text(wizard1().hp);
-    };
-    playerGenerator();
-  });
+  const wizard1 = storeState(wizard);
+  // $('#player-stats-hp').text(wizard1()10)
+  $('#player-stats-hp').text(wizard1().hp);
+  $('#player-stats-magic').text(wizard1().magic);
 
   $('#player-actions').click(function () {
-    wizards[0](blueFood);
+    wizard1(blueFood);
     console.log(wizards[0]());
-    $('#player-stats-hp').text(wizards[0]().hp);
-    $('#player-stats-magic').text(wizards[0]().magic);
-    $('#player-stats-water').text(wizards[0]().water);
-    $('#player-stats-hp').text(wizards[0]().hp);
+    $('#player-stats-hp').text(wizard1().hp);
+    $('#player-stats-magic').text(wizard1().magic);
+    $('#player-stats-water').text(wizard1().water);
+    $('#player-stats-hp').text(wizard1().hp);
   });
 });
